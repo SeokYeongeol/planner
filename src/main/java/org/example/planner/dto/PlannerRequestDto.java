c51f42e7a812883cda82353ce9ef415da1442dca
cheoni5417 <cheoni5417@naver.com>
2025-02-11 22:55:52 +0900

    fix : final 제거

--- src/main/java/org/example/planner/dto/PlannerRequestDto.java
+++ src/main/java/org/example/planner/dto/PlannerRequestDto.java
@@ -2,14 +2,12 @@ package org.example.planner.dto;
 
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
 @Getter
-@RequiredArgsConstructor
 public class PlannerRequestDto {
     @NotEmpty(message = "제목을 입력해주세요.")
-    private final String title;
+    private String title;
 
     @NotEmpty(message = "내용을 입력해주세요.")
-    private final String contents;
+    private String contents;
 }
