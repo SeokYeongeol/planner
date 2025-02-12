22145b2de44d29a8f09029444c9923a47de2bd11
cheoni5417 <cheoni5417@naver.com>
2025-02-12 20:28:23 +0900

    feat : username과 title의 글자수 제한 validation

--- src/main/java/org/example/planner/dto/PlannerRequestDto.java
+++ src/main/java/org/example/planner/dto/PlannerRequestDto.java
@@ -1,10 +1,12 @@
 package org.example.planner.dto;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class PlannerRequestDto {
+    @Size(max = 20, message = "제목의 글자수는 20보다 작아야 됩니다.")
     @NotEmpty(message = "제목을 입력해주세요.")
     private String title;
 
--- src/main/java/org/example/planner/dto/UserRequestDto.java
+++ src/main/java/org/example/planner/dto/UserRequestDto.java
@@ -2,10 +2,12 @@ package org.example.planner.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UserRequestDto {
+    @Size(max = 10, message = "이름의 글자수는 10보다 작아야 됩니다.")
     @NotBlank(message = "이름을 입력해주세요.")
     private String username;
 
